eff581e5862a00d59ce5967e97ec0ab8ae2e239a
Dinh-Duc Nguyen <ducn.nguyen@vmodev.com>
2019-09-03 19:33:15 +0700

    first commit

---
+++ src/demo/Main.java
@@ -0,0 +1,33 @@
+package demo;
+
+public class Main {
+    public static void main(String[] args) {
+//        //khai bao
+//        int soCanKT;
+//        //gan gia tri
+//        soCanKT = 6;
+//        boolean ketQua;
+//        ketQua = true;
+//        for ( int i = 2; i < soCanKT/2; i++){
+//            if (soCanKT % i == 0){
+//                ketQua= false;
+//            }
+//        }
+//        System.out.println(ketQua);
+
+
+
+        SNT sNT = new SNT();
+        sNT.soKT = 10;
+        sNT.kiemTra();
+        System.out.println("ket qua: " + sNT.ketQua);
+
+        SNT sNT2 = new SNT();
+        sNT2.soKT = 5;
+        sNT2.kiemTra();
+        System.out.println("ket qua: " + sNT2.ketQua);
+
+
+
+    }
+}
